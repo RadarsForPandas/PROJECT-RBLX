48ef58cd545f8fa015b96c09179877223ece6861
Panda <126839661+LuvinPandas@users.noreply.github.com>
2024-08-02 10:18:12 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,24 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OperationFeather/Pandera-Client-V1/main/Games/301549746.lua"))()
+-- DEBUG Prints are commmented
+local GamesFol = {
+    {
+        PlaceIDs = {301549746},
+        ScriptURL = "https://raw.githubusercontent.com/OperationFeather/Pandera-Client-V1/main/Games/301549746.lua"
+    }
+}
+
+local function loadGameScript(placeID)
+    for _, game in ipairs(GamesFol) do
+        for _, id in ipairs(game.PlaceIDs) do
+            if id == placeID then
+         --       print("Loading script for place ID: " .. id)
+                local scriptContent = game:HttpGet(game.ScriptURL)
+                loadstring(scriptContent)()
+                return
+            end
+        end
+    end
+--    print("No script found for place ID: " .. placeID)
+end
+
+local placeID = game.PlaceId
+loadGameScript(placeID)
